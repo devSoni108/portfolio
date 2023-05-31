4385ae421d3432fbc0939b4201c3550cbfc40b45
devSoni <soni.hiren108@gmail.com>
2023-05-31 17:56:16 +0200

    Updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,21 +8,21 @@ import './navbar.css';
 
 const Menu = () => (
   <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#about">About</a></p>
-  <p><a href="#projects">Projects</a></p>
-  <p><a href="#contact">Contact</a></p>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#about">About</a></p>
+    <p><a href="#projects">Projects</a></p>
+    <p><a href="#contact">Contact</a></p>
   </>
-)
+);
 
 const Navbar = () => {
-const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="hs__navbar" id="home">
       <div className="hs__navbar-links">
         <div className="hs__navbar-links_logo">
-          <a href="home"><img src={logo} alt="logo"/></a>
+          <a href="#home"><img src={logo} alt="logo" /></a>
         </div>
         <div className="hs__navbar-links_container">
           <Menu />
@@ -30,37 +30,44 @@ const [toggleMenu, setToggleMenu] = useState(false);
       </div>
 
       <div className="hs__navbar-social">
-        <a href="https://www.linkedin.com/in/hiren-soni-creator/" target="_blank">
+        <a href="https://www.linkedin.com/in/hiren-soni-creator/" target="_blank" rel="noreferrer">
           <img src={linkedin} alt="linkedin" />
         </a>
-        <a href="https://twitter.com/devsoni108/" target="_blank">
+        <a href="https://twitter.com/devsoni108/" target="_blank" rel="noreferrer">
           <img src={twitter} alt="twitter" />
         </a>
-        <a href="https://github.com/devSoni108/" target="_blank">
+        <a href="https://github.com/devSoni108/" target="_blank" rel="noreferrer">
           <img src={github} alt="github" />
         </a>
       </div>
 
       <div className="hs__navbar-menu">
-        {toggleMenu
-          ? <RiCloseLine color="fff" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color="fff" size={27} onClick={() => setToggleMenu(true)} />
-        }
+        {toggleMenu ? (
+          <RiCloseLine color="fff" size={27} onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu3Line color="fff" size={27} onClick={() => setToggleMenu(true)} />
+        )}
         {toggleMenu && (
           <div className="hs__navbar-menu_container scale-up-center">
             <div className="hs__navbar-menu_container-links">
               <Menu />
               <div className="hs__navbar-menu_container-links-social">
-                <img src={linkedin} alt="linkedin" href="https://www.linkedin.com/in/hiren-soni-creator/" />
-                <img src={twitter} alt="twitter" href="https://twitter.com/devsoni108" />
-                <img src={github} alt="github" href="https://github.com/devSoni108" />
+                <a href="https://www.linkedin.com/in/hiren-soni-creator/" target="_blank" rel="noreferrer">
+                  <img src={linkedin} alt="linkedin" />
+                </a>
+                <a href="https://twitter.com/devsoni108" target="_blank" rel="noreferrer">
+                  <img src={twitter} alt="twitter" />
+                </a>
+                <a href="https://github.com/devSoni108" target="_blank" rel="noreferrer">
+                  <img src={github} alt="github" />
+                </a>
               </div>
             </div>
           </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/containers/cta/CTA.jsx
+++ src/containers/cta/CTA.jsx
@@ -10,30 +10,25 @@ const CTA = () => {
     <div>
       <div className="hs__cta" id="contact">
         <div className="hs__cta-content">
-          <h3>Get in touch today & start<br/>exploring the endless possibilities</h3>
+          <h3>Get in touch today & start<br />exploring the endless possibilities</h3>
           <div className="hs__cta-social">
-            <a href="https://www.linkedin.com/in/hiren-soni-creator/" target="_blank">
+            <a href="https://www.linkedin.com/in/hiren-soni-creator/" target="_blank" rel="noreferrer">
               <img src={linkedin} alt="linkedin" />
             </a>
-            <a href="https://twitter.com/devsoni108/" target="_blank">
+            <a href="https://twitter.com/devsoni108/" target="_blank" rel="noreferrer">
               <img src={twitter} alt="twitter" />
             </a>
-            <a href="https://github.com/devSoni108/" target="_blank">
+            <a href="https://github.com/devSoni108/" target="_blank" rel="noreferrer">
               <img src={github} alt="github" />
             </a>
           </div>
         </div>
         <div className="hs__cta-content">
-          {/* <div className="hs__cta-btn">
-            <label><input/></label>
-            <button type="button">Get Notified</button>
-          </div>
-          <p>Subscribe for updates on new and exiciting projects</p> */}
           <p>Email: soni.hiren108@gmail.com</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
