e346b284ac3baa6230c77f2be6d72054b0c746f7
devSoni <soni.hiren108@gmail.com>
2023-05-31 17:46:40 +0200

    Build updates

--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Article, Modal } from '../../components';
 import { fitnessApp, zombie, ticketDesk, machinery } from './imports';
-import './projects.css';  
+import './projects.css';
 
 const Projects = () => {
   return (
@@ -13,68 +13,64 @@ const Projects = () => {
         <div className="hs__projects-container_groupB">
           <div className="hs__projects-container_groupA">
             <div className="hs__projects-container_project">
-              <Article imgUrl={fitnessApp} 
-              title="Only Fitness"
-              tools="ReactJS | Materiel UI"
-              description="Fast track your fitness journey with this easy to use fitness app. View exercises by target muscle or equipment." />
+              <Article
+                imgUrl={fitnessApp}
+                title="Only Fitness"
+                tools="ReactJS | Materiel UI"
+                description="Fast track your fitness journey with this easy to use fitness app. View exercises by target muscle or equipment."
+              />
               <div className="hs__projects-container_article-content">
-                <a href="https://github.com/devSoni108/only-fitness" target="_blank"><p>View Code</p></a>
-                <a href="https://only-fitness.netlify.app/" target="_blank"><p>View Application</p></a>
+                <a href="https://github.com/devSoni108/only-fitness" target="_blank" rel="noreferrer"><p>View Code</p></a>
+                <a href="https://only-fitness.netlify.app/" target="_blank" rel="noreferrer"><p>View Application</p></a>
               </div>
             </div>
           </div>
           <div className="hs__projects-container_groupA">
             <div className="hs__projects-container_project">
-              <Article imgUrl={machinery} 
+              <Article
+                imgUrl={machinery}
                 title="GTC Africa"
                 tools="NextJs | Tailwind"
-                description="GTC Africa is a prominent specialist in drivetrain components, and hydraulic pumps and motors. View their website to learn more." />
+                description="GTC Africa is a prominent specialist in drivetrain components, and hydraulic pumps and motors. View their website to learn more."
+              />
               <div className="hs__projects-container_article-content">
-                <a href="https://github.com/devSoni108/gtc-africa" target="_blank"><p>View Code</p></a>
-                <a href="https://gtccomponents.co.za/" target="_blank"><p>View Website</p></a>
+                <a href="https://github.com/devSoni108/gtc-africa" target="_blank" rel="noreferrer"><p>View Code</p></a>
+                <a href="https://gtccomponents.co.za/" target="_blank" rel="noreferrer"><p>View Website</p></a>
               </div>
             </div>
           </div>
           <div className="hs__projects-container_groupA">
             <div className="hs__projects-container_project">
-              <Article imgUrl={ticketDesk} 
-              title="Ticket Desk"
-              tools="PostgreSQL | Retool"
-              description="Full-stack web app to seamlessly manage tickets and improve response time with SMS and email alerts, just like the pros at Pager Duty!" />
+              <Article
+                imgUrl={ticketDesk}
+                title="Ticket Desk"
+                tools="PostgreSQL | Retool"
+                description="Full-stack web app to seamlessly manage tickets and improve response time with SMS and email alerts, just like the pros at Pager Duty!"
+              />
               <div className="hs__projects-container_article-content">
-                <a href="https://github.com/devSoni108/page-up" target="_blank"><p>View Code</p></a>
+                <a href="https://github.com/devSoni108/page-up" target="_blank" rel="noreferrer"><p>View Code</p></a>
                 <Modal />
               </div>
             </div>
           </div>
           <div className="hs__projects-container_groupA">
             <div className="hs__projects-container_project">
-              <Article imgUrl={zombie} 
-              title="Run Doggo"
-              tools="JavaScript | CSS"
-              description="Join Russel, the intrepid canine from Canada, as he tries to escape the creatures of the night and make it back home in this 2D JavaScript game." />
+              <Article
+                imgUrl={zombie}
+                title="Run Doggo"
+                tools="JavaScript | CSS"
+                description="Join Russel, the intrepid canine from Canada, as he tries to escape the creatures of the night and make it back home in this 2D JavaScript game."
+              />
               <div className="hs__projects-container_article-content">
-                <a href="https://github.com/devSoni108/run-doggo" target="_blank"><p>View Code</p></a>
-                <a href="https://run-doggo.netlify.app/" target="_blank"><p>Play Game Using Keyboard</p></a>
+                <a href="https://github.com/devSoni108/run-doggo" target="_blank" rel="noreferrer"><p>View Code</p></a>
+                <a href="https://run-doggo.netlify.app/" target="_blank" rel="noreferrer"><p>Play Game Using Keyboard</p></a>
               </div>
             </div>
           </div>
-          {/* <div className="hs__projects-container_groupA">
-            <div className="hs__projects-container_project">
-              <Article imgUrl={raag} 
-              title="Open AI Raag App"
-              tools="Python | OpenAI"
-              description="Discover the Magic of Hindustani Classical Music: Explore raags and taals with ease using our powerful web based application." />
-              <div className="hs__projects-container_article-content">
-                <a><p>Project Ongoing</p></a>
-                <a><p>Coming Soon</p></a>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
